f9bb867ddcfd6b7f0e9b87c5d33dec850df060ce
AYMAN HAQUE <aymanhaque001@gmail.com>
2020-02-21 19:37:07 -0600

    REACT WEBAPP

---
+++ src/Spring.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+
+function Spring() {
+  return (
+    <div>
+        <h1 className="header">Spring</h1>
+    
+    
+     <body className="gallery">
+        
+     <a href="/public/img/Spring/1.jpg" class="image">
+       <img src="img/Spring/1.jpg" alt=""></img>
+     </a>
+
+     <a href="/public/img/Spring/2.jpg" class="image">
+       <img src="img/Spring/2.jpg" alt=""></img>
+     </a>
+
+     <a href="/public/img/Spring/3.jpg" class="image">
+       <img src="img/Spring/3.jpg" alt=""></img>
+     </a>
+
+    
+
+ 
+
+
+ </body>
+
+ </div>
+
+
+  );
+}
+
+export default Spring;
---
+++ src/Summer.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+function Summer() {
+  return (
+    <div> 
+        <h1 className="header">Summer</h1>
+
+    
+      
+      <body className="gallery">
+        
+          <a href="/public/img/Summer/1.png" class="image">
+            <img src="img/Summer/1.jpg" alt=""></img>
+          </a>
+
+          <a href="/public/img/Summer/2.png" class="image">
+            <img src="img/Summer/2.jpg" alt=""></img>
+          </a>
+
+          <a href="/public/img/Summer/3.png" class="image">
+            <img src="img/Summer/3.jpg" alt=""></img>
+          </a>
+
+         
+
+      
+
+
+      </body>
+
+
+
+    </div>
+  );
+}
+
+export default Summer;
